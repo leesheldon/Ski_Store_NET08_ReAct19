b2d24b272f8902e979ace8c8c5ba983076267383
Sheldon <lhanco@gmail.com>
2025-03-18 13:08:23 +0700

    04_Implement RTK query. Create custom base query for data fetching. Add loading indicator. Use local storage and Redux to store Dark mode state.

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,6 +1,8 @@
 import { DarkMode, LightMode, ShoppingCart } from "@mui/icons-material";
-import { AppBar, Badge, Box, IconButton, List, ListItem, Toolbar, Typography } from "@mui/material";
+import { AppBar, Badge, Box, IconButton, LinearProgress, List, ListItem, Toolbar, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../store/store";
+import { setDarkMode } from "./uiSlice";
 
 const midLinks = [
     { title: 'catalog', path: '/catalog' },
@@ -25,19 +27,16 @@ const navStyles = {
     }
 };
 
-type Props = {
-    toggleDarkMode: () => void;
-    darkMode: boolean;
-};
-
-export default function NavBar({ darkMode, toggleDarkMode }: Props) {
+export default function NavBar() {
+    const {isLoading, darkMode} = useAppSelector(state => state.ui);
+    const dispatch = useAppDispatch();
 
     return (
         <AppBar position="fixed">
             <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
                 <Box display='flex' alignItems='center'>
                     <Typography variant="h6" component={NavLink} to='/' sx={navStyles}>SKI-STORE</Typography>
-                    <IconButton onClick={toggleDarkMode}>
+                    <IconButton onClick={() => dispatch(setDarkMode())}>
                         {darkMode ? <LightMode sx={{ color: 'yellow' }} /> : <DarkMode />}
                     </IconButton>
                 </Box>
@@ -76,6 +75,12 @@ export default function NavBar({ darkMode, toggleDarkMode }: Props) {
                     </List>
                 </Box>
             </Toolbar>
+
+            {isLoading && (
+                <Box sx={{width: '100%'}}>
+                    <LinearProgress color='secondary' />
+                </Box>
+            )}
         </AppBar>
     );
 }
