ebe4bd8564165398013870493e27dc3452b00170
Sheldon <lhanco@gmail.com>
2025-04-08 10:44:04 +0700

    11_Add Home page. Serve the app from the .Net server (wwwroot folder). Config the app to use SQL Server and for production build.

--- client/src/features/orders/OrderDetailsPage.tsx
+++ client/src/features/orders/OrderDetailsPage.tsx
@@ -101,7 +101,7 @@ export default function OrderDetailsPage() {
                                     x {item.quantity}
                                 </TableCell>
                                 <TableCell align='right' sx={{ p: 4 }}>
-                                    {currencyFormat(item.price)}
+                                    {currencyFormat(item.price * item.quantity)}
                                 </TableCell>
                             </TableRow>
                         ))}
